6856ad0ae00c5e2612c803575f04e6412db1c07d
Zarela Mae <74084073.zmvj@gmail.com>
2025-06-03 12:20:42 -0500

    Resuelve #1: Agrega consulta JOIN y subconsulta

--- archivo.sql
+++ archivo.sql
@@ -2,3 +2,14 @@
 select * from usuarios where id=5;
 =======
 
+-- JOIN entre las tablas empleados y departamentos
+SELECT e.nombre AS empleado, d.nombre AS departamento
+FROM empleados e
+JOIN departamentos d ON e.departamento_id = d.id;
+
+-- SUBCONSULTA para obtener empleados con salario mayor al promedio
+SELECT nombre, salario
+FROM empleados
+WHERE salario > (
+    SELECT AVG(salario) FROM empleados
+);
